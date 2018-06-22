435907a24f4b7ee32d141fc2c0e47f8d9eb558f2
Branden Pleines <bpleines@redhat.com>
2018-06-22 14:33:31 -0700

    testing

--- filter_plugins/custom.py
+++ filter_plugins/custom.py
@@ -2,8 +2,14 @@ def subnet_to_gateway(ipAddress):
     ip_components = ipAddress.split('.')
     return ip_components[0] + '.' +  ip_components[1] + '.' + ip_components[2]
 
+def strip_cidr(subnet):
+    ip_components = ipAddress.split('.')
+    final, cidr = ip_components[3].split('\\')
+    return ip_components[0] + '.' +  ip_components[1] + '.' + ip_components[2] + '.' + final
+
 class FilterModule(object):
     def filters(self):
         return {
-            'subnet_to_gateway': subnet_to_gateway
+            'subnet_to_gateway': subnet_to_gateway,
+            'strip_cidr': strip_cidr
             }
