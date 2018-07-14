fc6cde8176c8f72f13b777c7acc27289be2ac827
Branden Pleines <bpleines@Brandens-MacBook-Pro.local>
2018-07-13 17:54:27 -0700

    get rid of force_basic_auth

--- filter_plugins/custom.py
+++ filter_plugins/custom.py
@@ -2,13 +2,8 @@ def subnet_to_gateway(ipAddress):
     ip_components = ipAddress.split('.')
     return ip_components[0] + '.' +  ip_components[1] + '.' + ip_components[2]
 
-def strip_cidr(subnet):
-    ip_components = subnet.split('.')
-    return ip_components[0] + '.' +  ip_components[1] + '.' + ip_components[2] + '.' + '0'
-
 class FilterModule(object):
     def filters(self):
         return {
-            'subnet_to_gateway': subnet_to_gateway,
-            'strip_cidr': strip_cidr
+            'subnet_to_gateway': subnet_to_gateway
             }
